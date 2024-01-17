2766e0c49714b54ec304f954118c4ca9a977e738
Fernando Herrera <herrera770@outlook.es>
2024-01-17 14:35:02 -0300

    se agregó un título, el componente footer y el componente formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import { Container } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer";
+import FormularioTareas from "./components/FormularioTareas";
 
 function App() {
-  
   return (
     <>
-      
+      <Container>
+        <h1 className="text-center">Lista de Tareas</h1>
+        <FormularioTareas></FormularioTareas>
+      </Container>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
